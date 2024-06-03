83aa875b8efb0ba1cd270fa7a908a567f0c9b93e
Eason0319 <103716007+Eason0319@users.noreply.github.com>
2024-06-03 18:11:42 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -3,14 +3,21 @@ import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import Navigation from './src/navigation';
 import { GluestackUIProvider } from '@gluestack-ui/themed';
 import { config } from '@gluestack-ui/config';
+import { Provider } from 'react-redux';
+import store from './src/redux/store';
+import { ThemeProvider } from './src/Theme/themeProvider';
 
 const App = () => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <GluestackUIProvider config={config}>
-          <Navigation />
-      </GluestackUIProvider>
-    </SafeAreaView>
+    <ThemeProvider>
+    <Provider store={store}>
+        <SafeAreaView style={{ flex: 1 }}>
+          <GluestackUIProvider config={config}>
+            <Navigation />
+          </GluestackUIProvider>
+        </SafeAreaView>
+    </Provider></ThemeProvider>
+
   );
 }
 
